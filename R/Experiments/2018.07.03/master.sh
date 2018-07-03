32df15475a46606b0a72cbbf770c7f3dedb936cb
j1c <jaewonc78@gmail.com>
2018-07-03 17:09:25 -0400

    run rerf with bootstrap and max depth

---
+++ R/Experiments/2018.07.03/master.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH
+#SBATCH --job-name=rerf_subsample
+#SBATCH --array=1-23,25-106
+#SBATCH --time=3-0:0:0
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --mem=48G
+#SBATCH --partition=parallel
+#SBATCH --exclusive
+#SBATCH --mail-type=end
+#SBATCH --mail-user=jaewonc78@gmail.com
+
+module restore j1_env
+# Print this sub-job's task ID
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+# NAME_FILE=../../../Data/processed/names.txt
+NAME_FILE=~/work/jaewon/data/uci/processed/names.txt
+DATASET=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $NAME_FILE)
+
+sed "s/abalone/${DATASET}/g" run_benchmarks_2018_07_03.R > task${SLURM_ARRAY_TASK_ID}.R
+
+Rscript task${SLURM_ARRAY_TASK_ID}.R
+
+rm task${SLURM_ARRAY_TASK_ID}.R
+
+echo "job complete"
+
